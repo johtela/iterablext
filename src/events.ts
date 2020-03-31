ba7675248afafc8771f02c395742640afb20579b
tommijohtela <tommi.johtela@lowell.com>
2020-03-31 17:03:35 +0300

    Deleted unnecesary stuff

--- src/events.ts
+++ src/events.ts
@@ -3,13 +3,6 @@ export interface Event<M> {
     args: M[keyof M]
 }
 
-export interface ElemEvent extends Event<HTMLElementEventMap> { }
-
-let ev: ElemEvent = {
-    kind: "keydown",
-    args: null
-}
-
 export type EventStream<M> = AsyncIterable<Event<M>>
 
 export type ElemEventStream = EventStream<HTMLElementEventMap>
